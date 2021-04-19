4b43bbf2e22a69128a03969bd47a81e828ad1485
mariorodriguezdeleon@gmail.com <mariorodriguezdeleon@gmail.com>
2021-04-19 13:28:22 -0700

    fist assignment

---
+++ NewRandom/src/NewRandom.java
@@ -0,0 +1,12 @@
+/* Mario Rodriguez, CS 210
+ * April 19, 2021
+ * Bellevue College
+ * NewRandom class -
+ * Requirements:
+ */
+
+import java.util.Random;
+
+public class NewRandom extends Random {
+
+}
---
+++ NewRandom/src/NewRandomClient.java
@@ -0,0 +1,10 @@
+/* Mario Rodriguez, CS 210
+ * April 19, 2021
+ * Bellevue College
+ * NewRandom class Client -
+ * Requirements:
+ */
+
+public class NewRandomClient {
+
+}
