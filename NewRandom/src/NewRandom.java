b00da62abef8f380dd210db0930a5a8998cf1940
mariorodriguezdeleon@gmail.com <mariorodriguezdeleon@gmail.com>
2021-04-20 23:53:44 -0700

    final hw for assignmet 1

--- NewRandom/src/NewRandom.java
+++ NewRandom/src/NewRandom.java
@@ -1,12 +1,92 @@
 /* Mario Rodriguez, CS 210
+ * 980384835
  * April 19, 2021
  * Bellevue College
- * NewRandom class -
+ * NewRandom class - Extends the Java Random class.  Overloads the nextInt() method and provides additional methods as
+ * required for this assignment.
  * Requirements:
+ *   - Include an overload methode for nextInt()
+ *   - Include a nextEven method
+ *   - Include a nextOdd method
+ *   - Include a nextChar method
+ *   - Include a nextC
  */
 
 import java.util.Random;
 
 public class NewRandom extends Random {
 
+    // overloads Random's nextInt() to return a random number between low and high, both inclusive. n1
+    public int nextInt(int low, int high) {
+        int n = nextInt(high - low + 1) + low;
+        return n;
+    }
+
+    // returns an even random number between low and high, both inclusive. n2
+    public int nextEven(int low, int high) {
+        int n = 0;
+        boolean even = false;
+        while (!even) {
+            n = nextInt(low, high); //uses method from this class
+            if (n % 2 == 0) {
+                even = true;
+            }
+        }
+        return n;
+    }
+
+    // returns an odd random number between low and high, both inclusive. n3
+    public int nextOdd(int low, int high) {
+        int n = 0;
+        boolean odd = false;
+        while (!odd) {
+            n = nextInt(low, high); //uses method from this class
+            if (n % 2 != 0) {
+                odd = true;
+            }
+        }
+        return n;
+    }
+
+    // returns a random upper case "capital" character. n4
+    public char nextChar() {
+        int n = nextInt('A', 'Z');
+        char c = (char) n;
+        return c;
+    }
+
+    // returns a random upper case "capital" character. n5
+    public char nextChar(char from, char to) {
+
+        int n = nextInt(from, to);
+
+            if (n < to) {
+                return (char) n;
+            } else if (from > to) {
+                return (char) to;
+            } else {
+                return (char) n;
+            }
+
+        }
+
+    // returns a random special character. n6
+    public char nextSpecialChar(){
+
+        int low = 33;
+        int high = 126;
+        char c = ' ';
+        boolean special = false;
+
+        while (!special) {
+            int n = nextInt(low, high);
+            c = (char) n;
+
+            if (!Character.isDigit(c) && !Character.isLetter(c)) {
+                c = (char) n;
+                special = true;
+            }
+        }
+        return c;
+    }
 }
--- NewRandom/src/NewRandomClient.java
+++ NewRandom/src/NewRandomClient.java
@@ -1,10 +1,51 @@
 /* Mario Rodriguez, CS 210
  * April 19, 2021
  * Bellevue College
- * NewRandom class Client -
+ * NewRandom class Client
  * Requirements:
  */
 
 public class NewRandomClient {
+
+    public static void main (String[] args) {
+        NewRandom rand = new NewRandom();
+
+        System.out.println("n1) Test: nextInt(low, high)");
+        for (int i = 0; i < 6; i++) {
+            int a = rand.nextInt(3, 9);
+            System.out.println(a);
+        }
+
+        System.out.println("n2) Test: nextEven(low, high)");
+        for (int i = 0; i < 6; i++) {
+            int a = rand.nextEven(3, 9);
+            System.out.println(a);
+        }
+
+        System.out.println("n3) Test: nextOdd(low, high)");
+        for (int i = 0; i < 6; i++) {
+            int a = rand.nextOdd(3, 9);
+            System.out.println(a);
+        }
+
+        System.out.println("n4) Test: nextChar() random capital letter");
+        for (int i = 0; i < 6; i++) {
+            char a = rand.nextChar();
+            System.out.println(a);
+        }
+
+        System.out.println("n5) Test: nextChar(to, from) random capital letter too");
+        for (int i = 0; i < 6; i++) {
+            char a = rand.nextChar((char) 65, (char) 90);
+            System.out.println(a);
+        }
+
+        System.out.println("n6) Test: nextSpecialChar()");
+        for (int i = 0; i < 6; i++) {
+            char a = rand.nextSpecialChar();
+            System.out.println(a);
+        }
+
+    }
 
 }
