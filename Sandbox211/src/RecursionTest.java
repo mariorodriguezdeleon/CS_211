176e15c9967e74d65c6af1b85e9595d68da29296
mariorodriguezdeleon@gmail.com <mariorodriguezdeleon@gmail.com>
2021-11-08 12:00:51 -0800

    update1108

--- Sandbox211/src/RecursionTest.java
+++ Sandbox211/src/RecursionTest.java
@@ -4,7 +4,7 @@ public class RecursionTest {
         if (n > 0) {
             System.out.println(n);
             recursion(n - 1);
-        } else if (n )
+        }
     }
 
     public static void main (String[] args) {
